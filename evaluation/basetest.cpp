0f20bb4e17ca72a37dea67eea8bc0f8185ebd99c
Austin Wang <austinw@mit.edu>
2017-06-27 20:03:07 +0200

    Started work on base test

---
+++ evaluation/basetest.cpp
@@ -0,0 +1,25 @@
+/*
+ * basetest.cpp
+ *
+ *  Created on: Jun 27, 2017
+ *      Author: austin
+ */
+
+#include <opencv2/opencv.hpp>
+#include <string>
+#include <iostream>
+#include <sstream>
+#include <fstream>
+
+int main(int argc, char *argv[]) {
+	if(argc < 3) {
+		std::cout << "Usage ./basetest" << " path_to_descriptor_1 path_to_descriptor_2" << "\n";
+		return -1;
+	}
+
+	// match each descriptor in 1 to 2
+
+	//
+
+	return 0;
+}
